6af64950069b6019617f0fd4f34c4c72cd313576
Julien Liberta <julienliberta@stellium-mbp.home>
2025-04-03 17:29:17 +0200

    Add doc embedding prompt flow files

--- backend/PromptFlow/doc-embedding/add_2_index.py
+++ backend/PromptFlow/doc-embedding/add_2_index.py
@@ -1,51 +1,61 @@
 
 from promptflow.core import tool
-from promptflow.connections import CustomConnection, AzureOpenAIConnection
+from promptflow.connections import CustomConnection
 from azure.core.credentials import AzureKeyCredential
 from azure.search.documents import SearchClient
 from openai import AzureOpenAI
 import datetime
 
+
 # The inputs section will change based on the arguments of the tool function, after you save the code
 # Adding type to arguments and return value will help the system show the types properly
 # Please update the function name/signature per need
 @tool
-def my_python_tool(filename: str, input: list, searchconnection: CustomConnection, openai:AzureOpenAIConnection) -> list:
-    search_endpoint = searchconnection.endpoint
-    search_index = "legal-documents"
-    search_key = searchconnection.key
+def my_python_tool(filename: str, input: list, ally: CustomConnection) -> list:
+    search_endpoint = ally.search_endpoint
+    search_index = ally.search_document_index
+    search_key = ally.search_key
 
     # Create a client
     credential = AzureKeyCredential(search_key)
-    client = SearchClient(endpoint=search_endpoint,
+    search_client = SearchClient(endpoint=search_endpoint,
                         index_name=search_index,
                         credential=credential)
 
     client = AzureOpenAI(  
-        azure_endpoint=openai.api_base,  
-        api_key=openai.api_key,  
-        api_version="2024-08-01-preview"
+        azure_endpoint=ally.openai_endpoint,  
+        api_key=ally.openai_key,  
+        api_version=ally.openai_api_version
     )
 
     def text_embeding(text):
         import json
         response  = client.embeddings.create(
                 input = text,
-                model= "ada002"        
+                model = ally.openai_embedding_deployment       
         )
         json_data = json.loads(response.model_dump_json())
         return json_data['data'][0]['embedding']
         
     # for each item in input
-    for item in input['chunk']:
+    for item in input['chunks']:
         # add new field to item
-        item['id'] = filename + "-" + str(item['id'])
+        item['id'] = filename.strip().split(sep='.')[0] + "-" + str(item['id'])
         item['embedding'] = text_embeding(item['paragraph'])
         item['filename'] = filename
         # now in 2024-04-14T06:35:05Z format 
-        item['data'] = datetime.datetime.now().isoformat()
-    
-    # result = client.upload_documents(documents=
-    #                              [input['chunk']],)
+        item['date'] = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+        # Leave the properties below empty for now as functionality is not implemented
+        item['department'] = ""
+        item['group'] = []
+        item['isCompliant'] = False
+        item['CompliantCollection'] = []
+        item['NonCompliantCollection'] = []
 
-    return input['chunk']
+    try:
+        search_client.upload_documents(documents = input['chunks'])
+    except Exception as e:
+        print(f"Error documents to search index {search_index}: {e}")
+        return
+
+    return filename
--- backend/PromptFlow/doc-embedding/check_ifindexed.py
+++ backend/PromptFlow/doc-embedding/check_ifindexed.py
@@ -8,23 +8,29 @@ from azure.search.documents import SearchClient
 # Adding type to arguments and return value will help the system show the types properly
 # Please update the function name/signature per need
 @tool
-def list_policy_tool(filename:str, searchconnection: CustomConnection) -> object:
-    search_endpoint = searchconnection.endpoint
-    search_index = "legal-documents"
-    search_key = searchconnection.key
+def list_policy_tool(filename:str, ally: CustomConnection) -> object:
+    search_endpoint = ally.search_endpoint
+    search_index = ally.search_document_index
+    search_key = ally.search_key
     # use ai azure search to query 
 
     search_client = SearchClient(search_endpoint, search_index, AzureKeyCredential(search_key))
+    file_filter = "filename eq '{}'".format(filename)
+    filter = "({})".format(file_filter)   # Note this filter does no take the group into account
+
     results = search_client.search(
-        search_text=filename,  # Use '*' to match all documents
-        select="filename"     # Specify the fields to include in the results
+        select="filename",     # Specify the fields to include in the results
+        filter=filter,
+        include_total_count=True # Include the total count of documents in the results
     )
 
-    policy_list = []
-    for result in results:
-        policy_list.append({"filename": result["filename"]})
 
-    count = len(policy_list)
+    # policy_list = []
+    # for result in results:
+    #     policy_list.append({"filename": result["filename"]})
+
+    count = results.get_count()
+
     if count == 0:
         return 0 # not indexed
     else:
